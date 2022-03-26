795780d60dd8ffac7adc0ac5400dc25a63352bfe
Zachary Flota <zflota1@gmail.com>
2022-03-26 11:13:22 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers and Routers
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,5 +20,8 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listen for Connection
 app.listen(process.env.PORT)
+
+
 
