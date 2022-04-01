468eeaf8d2fca0695e7e041ada272bbb83a1705c
Zachary Flota <zflota1@gmail.com>
2022-04-01 19:37:06 -0400

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,7 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+//Add New Place
 router.post('/', (req, res) => {
   
   if (!req.body.pic) {
@@ -21,10 +22,12 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+//Index Page
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+//Show Page
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -38,6 +41,7 @@ router.get('/:id', (req, res) => {
   }
 })
 
+//Delete Page
 router.delete('/places/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -52,6 +56,7 @@ router.delete('/places/:id', (req, res) => {
   }
 })
 
+//Edit Page
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -62,6 +67,34 @@ router.get('/:id/edit', (req, res) => {
   }
   else {
     res.render('places/edit', { place: places[id] })
+  }
+})
+
+//Put Route
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
   }
 })
 
